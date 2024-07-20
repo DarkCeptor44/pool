1613e354f5876127f4966cbe11b1d08739a95f5c
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2024-07-20 17:40:40 -0300

    Fix worker; Add RunAndReturn

--- pool.go
+++ pool.go
@@ -38,9 +38,52 @@ func Run[K any](numWorkers int, values []K, job func(index int, value K)) error 
 	return nil
 }
 
+// RunAndReturn executes the task on a pool of workers of length numWorkers with the values
+// and returns the results
+func RunAndReturn[K any, V any](numWorkers int, values []K, job func(index int, value K) V) ([]V, error) {
+	if numWorkers <= 0 {
+		numWorkers = defaultNumWorkers
+	}
+
+	if len(values) == 0 {
+		return nil, errors.New("no values provided")
+	}
+
+	var wg sync.WaitGroup
+	data := make(chan K, BufferSize)
+	ret := make(chan V, BufferSize)
+
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go workerWithReturn(i, data, ret, job, &wg)
+	}
+
+	for _, val := range values {
+		data <- val
+	}
+
+	close(data)
+	close(ret)
+	wg.Wait()
+
+	results := make([]V, len(values))
+	for r := range ret {
+		results = append(results, r)
+	}
+
+	return results, nil
+}
+
 func worker[K any](index int, values <-chan K, job func(index int, value K), wg *sync.WaitGroup) {
+	defer wg.Done()
 	for val := range values {
 		job(index, val)
-		wg.Done()
+	}
+}
+
+func workerWithReturn[K any, V any](index int, values <-chan K, results chan<- V, job func(index int, value K) V, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for val := range values {
+		results <- job(index, val)
 	}
 }
--- pool_test.go
+++ pool_test.go
@@ -1,16 +1,43 @@
 package pool_test
 
 import (
+	"math/rand"
 	"slices"
 	"testing"
+	"time"
 
 	"github.com/DarkCeptor44/pool"
 )
+
+func TestNormal(t *testing.T) {
+	wrapTest(t, "Normal", func(i []int, f func(int)) {
+		for _, v := range i {
+			f(v)
+		}
+	})
+}
 
 func TestPool(t *testing.T) {
 	t.Run("NoValues", func(t *testing.T) {
 		err := pool.Run(10, []int{}, func(i int, v int) {
 			t.Log(i, v)
+		})
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+	})
+
+	wrapTest(t, "Pool", func(i []int, f func(int)) {
+		pool.Run(10, i, func(_, v int) {
+			f(v)
+		})
+	})
+}
+
+func TestPoolWithReturn(t *testing.T) {
+	t.Run("NoValues", func(t *testing.T) {
+		_, err := pool.RunAndReturn(10, []int{}, func(i int, v int) int {
+			return v * v
 		})
 		if err == nil {
 			t.Fatal("expected an error")
@@ -28,7 +55,7 @@ func BenchmarkNormal(b *testing.B) {
 
 func BenchmarkPool(b *testing.B) {
 	wrap(b, func(i []int, f func(int)) {
-		pool.Run(5, i, func(_, v int) {
+		pool.Run(10, i, func(_, v int) {
 			f(v)
 		})
 	})
@@ -47,4 +74,34 @@ func wrap(b *testing.B, f func([]int, func(int))) {
 			}
 		})
 	}
+}
+
+func wrapTest(t *testing.T, name string, f func([]int, func(int))) {
+	num := 5
+
+	t.Run(name, func(t *testing.T) {
+		var sum time.Duration
+
+		for i := 0; i < num; i++ {
+			vals := randSlice(10000, 100000)
+			start := time.Now()
+			f(vals, func(v int) {
+				if !slices.Contains(vals, v) {
+					t.Fatal("unexpected value")
+				}
+			})
+			sum += time.Since(start)
+		}
+
+		average := sum / time.Duration(num)
+		t.Logf("'%s' Took %s\n", name, average)
+	})
+}
+
+func randSlice(m, n int) []int {
+	vals := make([]int, n)
+	for i := 0; i < n; i++ {
+		vals[i] = rand.Intn(m)
+	}
+	return vals
 }
